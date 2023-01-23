10cff2ee6f582eeb6a47612e534d074a4bd1a442
tannazma <97254921+tannazma@users.noreply.github.com>
2023-01-23 17:19:04 +0100

    create the days and color the weekend

---
+++ date-helpers.js
@@ -0,0 +1,5 @@
+// function isWeekend(day) {
+//     return day % 7 === 0 || day % 7 === 6;
+// }
+
+// export { isWeekend };
---
+++ script.js
@@ -0,0 +1,18 @@
+// import { isWeekend } from "./date-helpers.js";
+function isWeekend(day) {
+    return day % 7 === 0 || day % 7 === 6;
+}
+
+const calendar = document.querySelector("#app-calendar");
+
+
+
+for (let day = 1; day <= 31; day++) {
+    const date = Date.now();
+    const options = { weekday: "short" };
+    const dayName =new Intl.DateTimeFormat("en-US" ,options).format(date);
+    console.log(dayName) 
+    const weekend = isWeekend(day)
+    console.log(weekend ? "weekend" : "")
+    calendar.insertAdjacentHTML("beforeend", `<div class="day ${weekend ? "weekend" : ""}">${day}</div>`);
+}
