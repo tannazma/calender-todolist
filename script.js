78f7131714761184607ff5e21d0b979e75bfedae
tannazma <97254921+tannazma@users.noreply.github.com>
2023-01-30 20:04:22 +0100

    revert back calculation of weekend and monday

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 let currentOpenDay;
 function isWeekend(day) {
-    return day % 7 === 1 || day % 7 === 0;
+    return day % 7 === 0 || day % 7 === 6;
 }
 
 const calendar = document.querySelector("#app-calendar");
 
 const getDayName = day => {
-    const date = new Date(Date.UTC(2023, 0, day));
+    const date = new Date(Date.UTC(2018, 0, day));
     return new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(date);
 }
 
