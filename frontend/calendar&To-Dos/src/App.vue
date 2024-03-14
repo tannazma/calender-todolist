3cd1e6f1650dd721cbf797e270b4b0af574ce856
tannazma <97254921+tannazma@users.noreply.github.com>
2024-03-14 18:12:56 +0100

    implement vue component

--- frontend/calendar&To-Dos/src/App.vue
+++ frontend/calendar&To-Dos/src/App.vue
@@ -1,17 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('Calendar + To-Dos')
+</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ message }}</h1>
 </template>
 
 <style scoped></style>
