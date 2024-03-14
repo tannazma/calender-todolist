bb5adf6e6572bb4b91d7030dda34c9430de78a1c
tannazma <97254921+tannazma@users.noreply.github.com>
2024-03-14 21:47:33 +0100

    implement template

--- frontend/calendar&To-Dos/src/App.vue
+++ frontend/calendar&To-Dos/src/App.vue
@@ -2,10 +2,30 @@
 import { ref } from 'vue'
 
 const message = ref('Calendar + To-Dos')
+const showDialog = ref(false)
+
+const openDialog = () => (showDialog.value = true)
+const closeDialog = () => (showDialog.value = false)
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+  <div id="vue-root"></div>
+  <h3 class="monthAndYear h4"></h3>
+  <div class="buttonContainer"></div>
+  <div id="app-calendar"></div>
+  <div class="background" id="buy-dialog" v-if="closeDialog">
+    <div class="dialogBox">
+      <div>
+        <input class="input-task input-event" placeholder="Add your tasks or events" />
+        <button @click="closeDialog" class="closeBtn btn btn-secondary">&times;</button>
+        <div class="taskEvent">
+          <button  class="add-task btn btn-primary btn-sm">Task</button>
+          <button  class="add-event btn btn-outline-primary">Event</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
